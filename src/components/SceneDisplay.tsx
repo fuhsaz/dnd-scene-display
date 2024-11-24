0428959156b366a3fbbe054b11890d77aecc4cb2
Ford <phordh@gmail.com>
2024-11-24 17:25:35 -0500

    Images now properly fit inside the display space

--- src/components/SceneDisplay.tsx
+++ src/components/SceneDisplay.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo } from "react";
 import { downloadImage } from "../service/image";
 import { SceneResponse } from "../types";
 
@@ -7,28 +7,50 @@ interface SceneDisplayProps {
 }
 
 export default function SceneDisplay({ scene }: SceneDisplayProps) {
-  const [imageUrl, setImageUrl] = useState<string>("");
+
+  const img = useMemo(() => document.createElement("img"), [])
+
+  img.alt = scene.name ?? "";
 
   useEffect(() => {
     (async () => {
       if (scene.url) {
-        setImageUrl(await downloadImage(scene.url ?? ""));
+        const url = await downloadImage(scene.url);
+        img.src = url;
+
+        const poll = setInterval(() => {
+          if (img.naturalWidth) {
+            clearInterval(poll);
+          }
+        }, 10);
+
+        const imageContainer = document.querySelector(".image-container");
+
+        const containerWidth = imageContainer?.clientWidth;
+        const containerHeight = imageContainer?.clientHeight;
+
+        img.style.maxHeight = `${containerHeight}px`;
+        img.style.maxWidth = `${containerWidth}px`;
+
+        img.style.objectFit = "contain";
+
+        if (scene.orientation === "landscape") {
+          img.style.width = "100%";
+        } else {
+          img.style.height = "100%";
+        }
+
+        imageContainer?.appendChild(img);
       }
     })();
-  }, [scene.url]);
+  }, [img, scene.url, scene.orientation]);
 
   return (
     <div className="container h-100">
       <div className="row h-100">
         <div className="col-sm-12 scene-container">
           <h1 className="text-center">{scene.name}</h1>
-          <div className="image-container">
-            <img
-              src={imageUrl}
-              alt={scene.name ?? ""}
-              className={`display-image ${scene.orientation ?? "portrait"}`}
-            />
-          </div>
+          <div className="image-container" />
         </div>
       </div>
     </div>
